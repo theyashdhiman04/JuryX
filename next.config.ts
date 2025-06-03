d8804ea66aa0e23cc1340f136332075c66310ad4
Yash Dhiman <theyashdhiman@gmail.com>
2025-06-03 22:41:58 +0530

    push next config

--- next.config.ts
+++ next.config.ts
@@ -83,6 +83,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes and static files
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+       
+        ],
+      },
+    ];
+  },
+  // Optional: Use standalone for better server deployment
+  output: "standalone",
 };
+
 
 export default nextConfig;
